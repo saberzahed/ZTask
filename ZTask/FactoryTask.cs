caefa27c65a93957011cb1ead5454088bbfeb2b1
SaberZahedianfard <saberzahedy@gmail.com>
2021-11-17 00:27:13 +0330

    Initial ZTask

---
+++ ZTask/FactoryTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ZTask
+{ public sealed class FactoryTask
+    {
+        private FactoryTask()
+        {
+                
+        }
+
+        internal static FactoryTask CreateInstance()
+        {
+            return new FactoryTask();
+        }
+
+        public IntervalJob StartNew(Action execution, TimeSpan interval)
+        {
+            var task = new IntervalJob(execution, interval);
+            task.Start();
+            return task;
+        }
+
+        public IntervalJob StartNew(Action execution, int second)
+        {
+            var task = new IntervalJob(execution, TimeSpan.FromSeconds(second));
+            task.Start();
+            return task;
+        }
+
+        public IntervalJob New(Action execution, TimeSpan interval) =>
+            new IntervalJob(execution, interval);
+
+        public IntervalJob New(Action execution, int second) =>
+            new IntervalJob(execution, TimeSpan.FromSeconds(second));
+    }
+  
+}
---
+++ ZTask/IntervalJob.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ZTask
+{
+    public class IntervalJob
+    {
+        private readonly TimeSpan _interval;
+        private readonly Task _task;
+        private readonly CancellationTokenSource _token;
+
+        public static FactoryTask Factory => FactoryTask.CreateInstance();
+
+        internal  IntervalJob(Action execution, TimeSpan interval)
+        {
+            _interval = interval;
+            _token = new CancellationTokenSource();
+            _task = new Task(() => Do(_token.Token, execution), TaskCreationOptions.LongRunning);
+        }
+
+        private void Do(CancellationToken cancellationToken, Action action)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                action();
+                Thread.Sleep(_interval);
+            }
+        }
+
+        public void Start()
+        {
+            if (_task.Status != TaskStatus.Created)
+                return;
+            _task.Start();
+        }
+
+        public async Task Stop()
+        {
+            _token?.Cancel();
+            await _task;
+            _task?.Dispose();
+        }
+    }
+}
